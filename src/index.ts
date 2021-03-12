946fdda7e1161216ef98786fdfc81b924f9f46e0
Cologler <skyoflw@gmail.com>
2021-03-13 03:05:57 +0800

    feat: jump with hash/exec command

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,57 @@ import joplin from 'api';
 import { ContentScriptType } from 'api/types';
 
 async function jump(url: URL) {
+	/**
+	 * examples:
+	 *   - jump://?query={query}#{hash}
+	 */
+
 	const query = url.searchParams.get('query');
+	let hash = url.hash;
+	if (hash) {
+		hash = hash.substr(1);
+	}
+
 	if (query) {
 		console.debug(`query: ${query}`);
 		const r = await joplin.data.get(['search'], {
-			fields: ['id', 'title'],
+			fields: ['id'],
 			query
 		})
 		console.debug(`notes count: ${r.items.length}`);
 		if (r.items.length > 0) {
-			joplin.commands.execute(
-				'openNote',
-				r.items[0].id
-			);
+			joplin.commands.execute('openNote', r.items[0].id, hash);
 		}
+	} else if (hash) {
+		joplin.commands.execute('scrollToHash', hash);
+	}
+}
+
+async function exec(url: URL) {
+	/**
+	 * examples:
+	 *   - exec://{command}?0=a&1=ds
+	 */
+
+	let command = url.pathname;
+
+	// remove `//` prefix:
+	if (command.startsWith('/')) command = command.substr(1);
+	if (command.startsWith('/')) command = command.substr(1);
+
+	const args = [];
+	for (let i = 0; i < 20; i++) {
+		const is = i.toString();
+		if (url.searchParams.has(is)) {
+			args.push(url.searchParams.get(is));
+		} else {
+			break;
+		}
+	}
+
+	if (command) {
+		console.debug(`exec ${command} with arguments: ${args}`);
+		joplin.commands.execute(command, ...args);
 	}
 }
 
@@ -31,6 +68,10 @@ joplin.plugins.register({
 			switch (url.protocol.toLowerCase()) {
 				case 'jump:':
 					jump(url);
+					break;
+
+				case 'exec:':
+					exec(url);
 					break;
 			}
 		});
